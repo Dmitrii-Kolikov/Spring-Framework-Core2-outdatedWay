7ef48481ca43de2d6f8135f9ea041f6fcb85d587
Dmitrii <74898966+Dmitrii-Kolikov@users.noreply.github.com>
2020-12-03 19:27:19 +0700

    Update ClassicalMusic.java

--- springcourse/ClassicalMusic.java
+++ springcourse/ClassicalMusic.java
@@ -1,6 +1,12 @@
 package ru.kolikov.springcourse;
 
 public class ClassicalMusic implements Music {
+     public void doMyInit() {
+        System.out.println("Doing my initialization");
+    }
+    public void doMyDestroy() {
+        System.out.println("Doing my destruction");
+    }
 
     @Override
     public String getSong() {
